ee0a5357fcc1922ce11f6191e8cec3511181af0c
Andrew Love <love.andrew.j@gmail.com>
2022-11-14 12:49:21 +0000

    Add text to charts, new build

--- src/partials/dashboard/DashboardCardIbmiAspUsed.jsx
+++ src/partials/dashboard/DashboardCardIbmiAspUsed.jsx
@@ -4,11 +4,14 @@ function DashboardCardIbmiAspUsed() {
 
  
   return (
-    <div className="flex-initial bg-white border rounded-sm shadow-lg h-96 col-span-full sm:col-span-10 xl:col-span-10 border-slate-200">
+    <div className="flex-initial bg-white border rounded-sm shadow-lg h-110 col-span-full sm:col-span-10 xl:col-span-10 border-slate-200">
       <div className="px-5 pt-5">
         <header className="flex items-start justify-between mb-2">
         </header>
         <h2 className="mb-2 text-lg font-semibold text-slate-800">ASP Used</h2>
+        <div className="flex items-start">
+          <div className="mb-2 text-slate-600">The total system storage pool capacity and amount currently in use.</div>
+        </div>
         <div className="inline-flex w-12 pt-0.5 px-1.5 mb-1 text-xs font-semibold text-white uppercase rounded-full bg-sky-500">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 28 28" stroke-width="2" stroke="currentColor" aria-hidden="true" class="-tr-ml-1 tr-mr-2.5 tr-h-4 tr-w-4">
             <path stroke-linecap="round" stroke-linejoin="round" d="M5.636 18.364a9 9 0 010-12.728m12.728 0a9 9 0 010 12.728m-9.9-2.829a5 5 0 010-7.07m7.072 0a5 5 0 010 7.07M13 12a1 1 0 11-2 0 1 1 0 012 0z">
@@ -17,7 +20,7 @@ function DashboardCardIbmiAspUsed() {
           {PLATFORM.environment}
         </div>
       </div>
-      <div className="grow">
+      <div className="mt-2 mb-2 grow">
         <div data-netdata="ibmi_remote.asp_utilisation" 
               data-host="http://localhost:19999/"
               data-chart-library="dygraph" 
--- src/partials/dashboard/DashboardCardIbmiAspUsedPercent.jsx
+++ src/partials/dashboard/DashboardCardIbmiAspUsedPercent.jsx
@@ -3,11 +3,14 @@ import React from 'react';
 function DashboardCardIbmiAspUsedPercent() {
 
   return (
-    <div className="flex-initial w-64 bg-white border rounded-sm shadow-lg h-96 col-span-full sm:col-span-5 xl:col-span-4 border-slate-200">
+    <div className="flex-initial bg-white border rounded-sm shadow-lg h-110 col-span-full sm:col-span-5 xl:col-span-4 border-slate-200">
       <div className="px-5 pt-5">
         <header className="flex items-start justify-between mb-2">
         </header>
         <h2 className="mb-2 text-lg font-semibold text-slate-800">ASP Utilisation</h2>
+        <div className="flex items-start">
+          <div className="mb-2 text-slate-600">The percentage of the system storage pool currently in use.</div>
+        </div>
         <div className="inline-flex w-12 pt-0.5 px-1.5 mb-1 text-xs font-semibold text-white uppercase rounded-full bg-sky-500">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 28 28" stroke-width="2" stroke="currentColor" aria-hidden="true" class="-tr-ml-1 tr-mr-2.5 tr-h-4 tr-w-4">
             <path stroke-linecap="round" stroke-linejoin="round" d="M5.636 18.364a9 9 0 010-12.728m12.728 0a9 9 0 010 12.728m-9.9-2.829a5 5 0 010-7.07m7.072 0a5 5 0 010 7.07M13 12a1 1 0 11-2 0 1 1 0 012 0z">
@@ -16,7 +19,7 @@ function DashboardCardIbmiAspUsedPercent() {
           {PLATFORM.environment}
         </div>
       </div>
-      <div className="grow">
+      <div className="ml-20 grow">
         <div data-netdata="ibmi_remote.asp_used_percent" 
               data-host="http://localhost:19999/"
               data-chart-library="easypiechart" 
